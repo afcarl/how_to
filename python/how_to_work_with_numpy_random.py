4d516b76152c9fb93d6df593615b804185a280b2
Max Lu <luyiren@seas.upenn.edu>
2017-01-28 14:36:39 -0500

    numpy random

--- python/how_to_work_with_numpy_random.py
+++ python/how_to_work_with_numpy_random.py
@@ -1,5 +1,7 @@
 ## How to work with numpy.random
 
+# numpy.random doc:
+# https://docs.scipy.org/doc/numpy/reference/routines.random.html
 import numpy as np
 
 # Randomly sample from a given distribution:
@@ -10,3 +12,11 @@ np.random.choice([1,2,4], p=[0.2,0.5,0.3])
 # Randomly sample a float from [0,1)
 # Example:
 np.random.random()
+
+# Randomly sample a integer from low (inclusive) to high (exclusive).
+#   randint(low[, high, size, dtype])
+# Example:
+np.random.randint(1, 10, 1)
+# returns: array([9])
+np.random.randint(1, 10)
+# returns: 7
